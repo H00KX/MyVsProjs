c1ac2d56a835d0f1d4544c741ffce7733cd931b8
dazhilang <dazhilang@outlook.com>
2018-11-25 11:33:46 +0800

    25Hw

---
+++ CR32_2C++homework/11_25/base64_ec_dc/MainUi.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "base64.h"
+
+int main()
+{
+    const std::string MyStr = "qwerasdf";
+
+    std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(MyStr.c_str()), MyStr.length());
+    std::string decoded = base64_decode(encoded);
+
+
+    std::cout << "encoded :" << std::endl << encoded << std::endl;
+
+    std::cout << "decoded :" << std::endl << decoded << std::endl;
+
+    system("pause");
+}
