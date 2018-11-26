016d66fecdaed6fa25330929bb968521c0b71a18
dazhilang <dazhilang@outlook.com>
2018-11-26 22:07:38 +0800

    完善

--- CR32_2C++homework/11_26/class_8/class_8.cpp
+++ CR32_2C++homework/11_26/class_8/class_8.cpp
@@ -3,9 +3,32 @@
 
 #include "stdafx.h"
 
+class Test
+{
+private:
+    int nVar;
+public:
+    void tFun(int a);
+};
+
+
+typedef void(Test::*PFN_tFun)(int a);
+
 
 int main()
 {
+    Test t;
+    PFN_tFun pfn;
+    pfn = &Test::tFun;
+
+    t.tFun(1);
+
+    (t.*pfn)(2);
+
     return 0;
 }
 
+void Test::tFun(int a)
+{
+    nVar = a;
+}
