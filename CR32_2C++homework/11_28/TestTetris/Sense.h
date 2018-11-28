51e8d28cfcd527328adf441c72bfe92c2095fc13
dazhilang <dazhilang@outlook.com>
2018-11-28 17:31:43 +0800

    copy Tetris

---
+++ CR32_2C++homework/11_28/TestTetris/Sense.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <memory>
+#include "Block.h"
+#include "setdisplay.h"
+
+using  namespace std;
+
+class CSense
+{
+public:
+    void Init();
+
+    void DrawBg();
+
+    void ShowBlock(int x, int y);
+    void ShowBg(int x, int y);
+
+
+private:
+    //char* m_pMap;
+
+    std::shared_ptr<char> m_pMap;
+
+    int m_nMapWidth;
+    int m_nMapHeight;
+
+    CBlock m_Block;
+    int  m_nCurX;
+    int m_nCurY;
+
+};
+
